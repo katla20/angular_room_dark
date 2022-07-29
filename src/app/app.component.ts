e2079a15df4d64b86f3af1ccd5f3b8d2c664dd94
katla20 <katla7775@gmail.com>
2022-07-28 21:24:17 -0500

    feature: include data in controller

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component, VERSION } from '@angular/core';
+import { cellState } from './interfaces/data.interface';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+  public map_grid: Array<Array<cellState>> = [
+    // cellState
+    // bulb (true o false)
+    // light (true o false)
+    // wall (true o false)
+
+    [
+      { light: false, bulb: false, wall: false },
+      { light: true, bulb: false, wall: false },
+      { light: true, bulb: false, wall: false },
+    ],
+    [
+      { light: false, bulb: false, wall: true },
+      { light: true, bulb: true, wall: false },
+      { light: false, bulb: false, wall: true },
+      { light: false, bulb: false, wall: true },
+    ],
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    console.log('holaaa');
+    //debugger
+  }
 }
---
+++ src/app/interfaces/data.interface.ts
@@ -0,0 +1,66 @@
+export interface patterns {
+  name: string;
+  iterative?: typeSecuential;
+  dataRows: Array<patternRow> | patternRow;
+  cntMaxRows: number;
+  cntMaxColumm: number;
+}
+
+export interface patternRow {
+  valueColumm?: number;
+  valueRow?: number;
+
+  cntColumm: number;
+  cntRow?: number;
+
+  numberRow?: number;
+  numberColumn?: number;
+
+  sequential?: typeSecuential;
+}
+
+export interface typeSecuential {
+  column?: direction;
+  row?: direction;
+}
+
+export interface direction {
+  reverse: boolean;
+  priority: boolean;
+}
+
+export interface matrix {
+  positions: Array<positions>;
+  cntRow: number;
+  cntColumn: number;
+  dataOrigin: Array<Array<any>>;
+  cntBulbs: number;
+}
+
+export interface positions {
+  idxColumm: number;
+  idxRow: number;
+  valueColumn: number;
+  valueRow: number;
+  wall: boolean | 0 | 1;
+  bulb: boolean;
+  withLight: boolean;
+  adjacentPos: nexPositionsMatrix;
+}
+
+export interface cellState {
+  wall: boolean | 0 | 1;
+  bulb: boolean;
+  light: boolean;
+}
+
+export interface allData {
+  data: Array<Array<cellState>>;
+}
+
+export interface nexPositionsMatrix {
+  up: Array<positions>;
+  down: Array<positions>;
+  rigth: Array<positions>;
+  left: Array<positions>;
+}
