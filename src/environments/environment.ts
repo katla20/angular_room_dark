27546c34d32ee1c0d88e189d1a37218610325946
katla20 <katla7775@gmail.com>
2022-08-01 00:12:21 -0500

    1 file modified

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   API_URL: 'http://127.0.0.1:8000/',
   user: {
-    username: 'greenchain@info.com',
+    email: 'greenchain@info.com',
     password: 'password',
   },
 };
